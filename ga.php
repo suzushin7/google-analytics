42f8c6c10a9fc2650ab370268fcb137b9878bf56
Shingo Suzuki <suzushinlab@gmail.com>
2021-08-04 18:23:24 +0900

    Create a new file
    version 1.00
    My first implementation.

---
+++ ga.php
@@ -0,0 +1,19 @@
+<!--
+Please input your information at corresponding positions.
+【1】: Your IP Address
+【2】: Your 測定ID (Sokukei ID)
+-->
+
+<?php 
+$address = "【1】";
+if($_SERVER["REMOTE_ADDR"] != $address) { ?>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=【2】"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', '【2】');
+</script>
+<?php } %>
